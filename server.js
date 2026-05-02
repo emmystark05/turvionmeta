6311e6bd9286fc80de1c4e5d1eb39525f1628778
emmystark <nkereuwememmanuel03@gmail.com>
2026-05-02 06:47:17 +0100

    updated-commit

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 const session = require('express-session');
-const sqlite3 = require('sqlite3').verbose();
+const Database = require('better-sqlite3');
+const db = new Database(path.join(__dirname, 'app.db'));
 const bcrypt = require('bcryptjs');
 const path = require('path');
 const http = require('http');             // NEW
